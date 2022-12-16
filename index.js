4380fa82e9a74b8640e7263b37721ea96cacffb7
Ari <arbo77@gmail.com>
2022-12-16 09:04:23 +0700

    update cors

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 // @arbo77
 const express = require('express');
 const fs = require('fs');
+const cors = require('cors');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const configFile = './config.json';
 
 const port = process.argv[2] || 3000;
 const app = express();
-let server;
+const whiteListIp = []
 
+app.use(cors({
+  methods: ["HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE"],
+  origin: "*",
+}));
+let server;
 
 function RunProxy() {
   server && server.close()
@@ -20,7 +26,10 @@ function RunProxy() {
       createProxyMiddleware({
         target: c.target,
         changeOrigin: true,
-        pathRewrite: (path, req) => { return path.replace(c.path, '/') }
+        pathRewrite: (path, req) => { return path.replace(c.path, '') },
+        onProxyRes: (proxyRes, req, res) => {
+          proxyRes.headers['x-proxy'] = 'dev-proxy'
+        },
       })
     );
   });
